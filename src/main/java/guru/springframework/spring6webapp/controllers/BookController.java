8afc79ac01f724489a466a978355e3fb355bb263
Khilna Gudhka <khilnahgudhka@gmail.com>
2024-08-08 20:06:47 +0530

    adding book controller

---
+++ src/main/java/guru/springframework/spring6webapp/controllers/BookController.java
@@ -0,0 +1,27 @@
+package guru.springframework.spring6webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.spring6webapp.services.BookService;
+
+@Controller
+public class BookController {
+
+    // new branch created - 9-add-controller
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookService.findAll());
+        return "books";
+    }
+
+}
